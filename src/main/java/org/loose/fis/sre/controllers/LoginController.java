42dd07c44c41458f20cb63917421f960c83df092
Alex Banu <alexandru.banu@student.upt.ro>
2022-05-21 14:10:48 +0300

    FOASC-3: Imported features from FOASC-4

---
+++ src/main/java/org/loose/fis/sre/controllers/LoginController.java
@@ -0,0 +1,50 @@
+package org.loose.fis.sre.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+
+import javax.swing.text.html.ImageView;
+
+public class LoginController {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private Label createAccount;
+
+    @FXML
+    private TextField userText;
+
+    @FXML
+    private PasswordField passwordText;
+
+    @FXML
+    public void loginButtonClicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    public void homeButtonClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    public void menuButtonClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    public void chatButtonClicked(ActionEvent event){
+
+    }
+
+    @FXML
+    public void createAccountClicked(ActionEvent event){
+
+    }
+}
