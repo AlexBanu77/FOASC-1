48ac54a7ae7fb2280fc878f1a59b57acd29af9a6
Alex Banu <alexandru.banu@student.upt.ro>
2022-05-21 09:47:03 +0300

    Added Login Page

---
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -0,0 +1,27 @@
+package org.loose.fis.sre;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.loose.fis.sre.services.FileSystemService;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.setFullScreen(true);
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 1920, 1080));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
